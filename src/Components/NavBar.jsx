e56f8ec5040eb5f895235bd98dc5f4e1196e664a
kirankumarkm08 <kirankumarkm1505@gmail.com>
2024-08-06 12:59:49 +0530

    suspense updated

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,7 +5,7 @@ import { Suspense } from "react";
 export const NavBar = () => {
   return (
     <div className=" w-full justify-center items-center flex p-4 dark:bg-gray-400 bg-amber-400 text-lg gap-6 ">
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense>
         <NavItems title="Trending" param={"fetchTrending"} />
         <NavItems title="Top Rated" param={"fetchToprated"} />
       </Suspense>
--- src/Components/NavItems.jsx
+++ src/Components/NavItems.jsx
@@ -3,21 +3,24 @@
 import React from "react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 const NavItems = ({ title, param }) => {
   const searchParams = useSearchParams();
   const genre = searchParams.get("genre");
   return (
-    <Link
-      className={`${
-        genre === param
-          ? "underline underline-offset-8 decoration-4 rounded-md dark:decoration-amber-500 "
-          : ""
-      }`}
-      href={`/?genre=${param}`}
-    >
-      {title}
-    </Link>
+    <Suspense>
+      <Link
+        className={`${
+          genre === param
+            ? "underline underline-offset-8 decoration-4 rounded-md dark:decoration-amber-500 "
+            : ""
+        }`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
+    </Suspense>
   );
 };
 
