447d9c5872985bb8b443e63cc9fc0885f2cfb460
kirankumarkm08 <kirankumarkm1505@gmail.com>
2024-08-06 11:45:04 +0530

    pushed

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import NavItems from "./NavItems";
+import { Suspense } from "react";
 
 export const NavBar = () => {
   return (
     <div className=" w-full justify-center items-center flex p-4 dark:bg-gray-400 bg-amber-400 text-lg gap-6 ">
-      <NavItems title="Trending" param={"fetchTrending"} />
-      <NavItems title="Top Rated" param={"fetchToprated"} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <NavItems title="Trending" param={"fetchTrending"} />
+        <NavItems title="Top Rated" param={"fetchToprated"} />
+      </Suspense>
     </div>
   );
 };
